f92cacc1648af0f066df35f00803cadff9ae0f4a
marianela santos <marianelasantoscusicanqui1234@gmail.com>
2024-07-15 16:32:40 -0400

    se agrego la version veta de la torre de hanoi

---
+++ TorreDeHanoi/TorreDeHanoi/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TorreDeHanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblMovimientos = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(174, 121);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 270);
+            this.panel1.TabIndex = 0;
+            this.panel1.Tag = "0";
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(15, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 42);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(48, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 35);
+            this.button4.TabIndex = 6;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(3, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 47);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(34, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 40);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(379, 121);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(176, 279);
+            this.panel2.TabIndex = 1;
+            this.panel2.Tag = "1";
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(585, 121);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(168, 270);
+            this.panel3.TabIndex = 2;
+            this.panel3.Tag = "2";
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.Gray;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnIniciar.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Location = new System.Drawing.Point(22, 27);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(139, 55);
+            this.btnIniciar.TabIndex = 7;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblMovimientos
+            // 
+            this.lblMovimientos.AutoSize = true;
+            this.lblMovimientos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblMovimientos.Font = new System.Drawing.Font("Goudy Stout", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovimientos.Location = new System.Drawing.Point(185, 27);
+            this.lblMovimientos.Name = "lblMovimientos";
+            this.lblMovimientos.Size = new System.Drawing.Size(230, 22);
+            this.lblMovimientos.TabIndex = 8;
+            this.lblMovimientos.Text = "Movimientos";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Ravie", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Transparent;
+            this.button5.Location = new System.Drawing.Point(22, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 58);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "REINICIAR";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(911, 490);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.lblMovimientos);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblMovimientos;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ TorreDeHanoi/TorreDeHanoi/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorreDeHanoi
+{
+    public partial class Form1 : Form
+    {
+        private int movimientos;
+        private Stack<Control>[] torres;
+        private int torreSeleccionada = -1;
+        private bool juegoCompletado = false;
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarTorres();
+            button5.Click += button5_Click;
+        }
+
+        private void InicializarTorres()
+        {
+            // Remover los botones de los paneles
+            foreach (var panel in new Panel[] { panel1, panel2, panel3 })
+            {
+                panel.Controls.Clear();
+            }
+
+            // Agregar los botones al Panel 1
+            panel1.Controls.AddRange(new Control[] { button1, button2, button3, button4 });
+
+            // Reiniciar la pila de torres
+            torres = new Stack<Control>[3];
+            torres[0] = new Stack<Control>();
+            torres[1] = new Stack<Control>();
+            torres[2] = new Stack<Control>();
+
+            // Agregar los botones a la primera torre (panel1)
+            torres[0].Push(button1);
+            torres[0].Push(button2);
+            torres[0].Push(button3);
+            torres[0].Push(button4);
+
+            // Reiniciar contador de movimientos
+            movimientos = 0;
+            lblMovimientos.Text = "Movimientos: 0";
+
+            // Reiniciar estado del juego completado
+            juegoCompletado = false;
+        }
+
+        private void panel_Click(object sender, EventArgs e)
+        {
+            Panel panel = sender as Panel;
+            int torre = int.Parse(panel.Tag.ToString());
+
+            if (torreSeleccionada == -1)
+            {
+                torreSeleccionada = torre;
+            }
+            else
+            {
+                MoverDisco(torreSeleccionada, torre);
+                torreSeleccionada = -1;
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            InicializarTorres();
+        }
+
+        private void MoverDisco(int origen, int destino)
+        {
+            // Verificar si el juego ya se ha completado
+            if (juegoCompletado)
+            {
+                // Restablecer el estado del juego
+                InicializarTorres();
+                juegoCompletado = false;
+                return;
+            }
+            if (torres[origen].Count == 0 || (torres[destino].Count > 0 && torres[origen].Peek().Width > torres[destino].Peek().Width))
+            {
+                MessageBox.Show("Movimiento inválido");
+                return;
+            }
+
+            Control disco = torres[origen].Pop();
+            torres[destino].Push(disco);
+
+            // Actualizar la posición del disco
+            switch (destino)
+            {
+                case 0:
+                    disco.Parent = panel1;
+                    break;
+                case 1:
+                    disco.Parent = panel2;
+                    break;
+                case 2:
+                    disco.Parent = panel3;
+                    break;
+            }
+
+            disco.BringToFront();
+
+            movimientos++;
+            lblMovimientos.Text = "Movimientos: " + movimientos;
+            // Verificar si el juego ha sido completado
+            if (torres[2].Count == 4) // Cambia 4 por el número total de botones
+            {
+                MessageBox.Show("¡Felicidades! Has completado el juego.");
+                juegoCompletado = true;
+            }
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            panel_Click(sender, e);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Este método puede permanecer vacío si no necesitas pintar nada específico en el panel.
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            InicializarTorres();
+        }
+    }
+}
